72830bee7d19d46ea6f0934745e5f7b7e67292eb
Jan Kropiwnicki <jan.kropiwnicki@infotech.edu.pl>
2025-09-03 09:06:00 +0200

    Extreacted PlayerSide component

--- app/clock.tsx
+++ app/clock.tsx
@@ -1,9 +1,10 @@
 import {router, Stack, useLocalSearchParams} from "expo-router";
 import * as ScreenOrientation from 'expo-screen-orientation'
-import {Pressable, Text, View} from "react-native";
+import {Pressable, Text, View, StyleSheet} from "react-native";
 import {useEffect, useState} from "react";
 import {useTimer} from 'react-timer-hook';
 import {useTimerResultType} from "react-timer-hook/dist/types/src/useTimer";
+import {useKeepAwake} from "expo-keep-awake";
 
 enum Player {
     NONE = 0,
@@ -11,32 +12,74 @@ enum Player {
     RIGHT = 2,
 }
 
-export default function ClockPage() {
-    const { seconds } = useLocalSearchParams();
+type PlayerSideProps = {
+    active: boolean;
+    initialTime: Date;
+    onPress: () => void;
+}
 
-    const time = new Date();
-    time.setSeconds(time.getSeconds() + parseInt(seconds as string));
-
-    const leftTimer = useTimer({ expiryTimestamp: time, autoStart: false, interval: 20 });
-    const rightTimer = useTimer({ expiryTimestamp: time, autoStart: false, interval: 20 });
-
-    const [player, setPlayer] = useState<Player>(Player.NONE);
+function PlayerSide(props: PlayerSideProps) {
+    const timer = useTimer({
+        expiryTimestamp: props.initialTime,
+        autoStart: false,
+        interval: 20,
+    });
 
     function getTimeString(timer: useTimerResultType) {
         return (
             String(timer.minutes).padStart(2, "0") + ':' +
             String(timer.seconds).padStart(2, "0")
-        )
+        );
     }
 
-    function setPlayerAndToggleTimers(p: Player) {
-        setPlayer(p)
-        if (p === Player.RIGHT) {
-            leftTimer.pause()
-            rightTimer.resume()
+    useEffect(() => {
+        if (props.active) {
+            timer.resume();
         } else {
-            rightTimer.pause()
-            leftTimer.resume()
+            timer.pause();
+        }
+    }, [props.active]);
+
+    return (
+        <Pressable
+            onPress={props.onPress}
+            onLongPress={() => router.navigate('/')}
+            style={[
+                styles.playerContainer,
+                { backgroundColor: props.active ? '#008000': '#ffffff' },
+            ]}
+        >
+            <View style={{
+                transform: [{'rotate': '90deg'}]
+            }}>
+                <Text style={{
+                    color: props.active ? '#ffffff' : '#008000',
+                    fontSize: 96
+                }}>
+                    { getTimeString(timer) }
+                </Text>
+            </View>
+        </Pressable>
+    )
+}
+
+export default function ClockPage() {
+    useKeepAwake();
+
+    const { seconds } = useLocalSearchParams();
+
+    const time = new Date();
+    time.setSeconds(time.getSeconds() + parseInt(seconds as string));
+
+    const [player, setPlayer] = useState<Player>(Player.NONE);
+
+    function onPlayerPress(p: Player) {
+        if (player != p && player != Player.NONE) return;
+
+        if (player == Player.LEFT) {
+            setPlayer(Player.RIGHT)
+        } else {
+            setPlayer(Player.LEFT)
         }
     }
 
@@ -52,47 +95,25 @@ export default function ClockPage() {
             <View style={{
                 flex: 1
             }}>
-                <Pressable
-                    onPress={() => setPlayerAndToggleTimers(Player.RIGHT)}
-                    onLongPress={() => router.navigate('/')}
-                    style={{
-                        flex: 1,
-                        backgroundColor: player === Player.LEFT ? '#008000' : '#ffffff',
-                        justifyContent: 'center',
-                        alignItems: 'center',
-                    }}
-                >
-                    <View style={{ transform: [{'rotate': '90deg'}] }}>
-                        <Text style={{
-                            color: player === Player.LEFT ? '#ffffff' : '#008000',
-                            transform: [{'rotate': '90deg'}],
-                            fontSize: 96
-                        }}>
-                            { getTimeString(leftTimer) }
-                        </Text>
-                    </View>
-                </Pressable>
-                <Pressable
-                    onPress={() => setPlayerAndToggleTimers(Player.LEFT)}
-                    onLongPress={() => router.navigate('/')}
-                    style={{
-                        flex: 1,
-                        backgroundColor: player === Player.RIGHT ? '#008000' : '#ffffff',
-                        justifyContent: 'center',
-                        alignItems: 'center',
-                    }}
-                >
-                    <View style={{ transform: [{'rotate': '90deg'}] }}>
-                        <Text style={{
-                            color: player === Player.RIGHT ? '#ffffff' : '#008000',
-                            transform: [{'rotate': '90deg'}],
-                            fontSize: 96
-                        }}>
-                            { getTimeString(rightTimer) }
-                        </Text>
-                    </View>
-                </Pressable>
+                <PlayerSide
+                    active={player == Player.LEFT}
+                    initialTime={time}
+                    onPress={() => onPlayerPress(Player.LEFT)}
+                />
+                <PlayerSide
+                    active={player == Player.RIGHT}
+                    initialTime={time}
+                    onPress={() => onPlayerPress(Player.RIGHT)}
+                />
             </View>
         </>
     )
 }
+
+const styles = StyleSheet.create({
+    playerContainer: {
+        flex: 1,
+        justifyContent: 'center',
+        alignItems: 'center',
+    }
+})
